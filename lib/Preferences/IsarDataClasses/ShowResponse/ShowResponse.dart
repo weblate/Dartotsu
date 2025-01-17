f892b1baac61699dd2857696a9627db9931ff5d3
aayush262 <aayushthakur262006@gmail.com>
2025-01-17 20:09:41 +0530

    [build.ios] bringing the crash back

--- lib/Preferences/IsarDataClasses/ShowResponse/ShowResponse.dart
+++ lib/Preferences/IsarDataClasses/ShowResponse/ShowResponse.dart
@@ -1,33 +1,25 @@
-import 'package:hive/hive.dart';
 
+import 'package:isar/isar.dart';
 part 'ShowResponse.g.dart';
 
-@HiveType(typeId: 0)
+@collection
 class ShowResponse {
-  @HiveField(0)
+  Id id = Isar.autoIncrement;
+
+  @Index(unique: true, replace: true)
+  late String key;
+
   final String name;
-
-  @HiveField(1)
   final String link;
-
-  @HiveField(2)
   final String coverUrl;
-
-  @HiveField(3)
   final List<String> otherNames;
-
-  @HiveField(4)
   final int? total;
-
-  @HiveField(5)
-  final Map<String, String>? extra;
 
   ShowResponse({
     required this.name,
     required this.link,
     required this.coverUrl,
     this.otherNames = const [],
-    this.total,
-    this.extra,
+    this.total
   });
 }
--- lib/Preferences/PrefManager.dart
+++ lib/Preferences/PrefManager.dart
@@ -1,26 +1,31 @@
-import 'package:dantotsu/Preferences/IsarDataClasses/DefaultPlayerSettings/DefaultPlayerSettings.dart';
-import 'package:dantotsu/Preferences/IsarDataClasses/Selected/Selected.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'dart:async';
 
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:isar/isar.dart';
 import '../StorageProvider.dart';
+import 'IsarDataClasses/DefaultPlayerSettings/DefaultPlayerSettings.dart';
 import 'IsarDataClasses/MalToken/MalToken.dart';
+import 'IsarDataClasses/Selected/Selected.dart';
 import 'IsarDataClasses/ShowResponse/ShowResponse.dart';
+import 'IsarDataClasses/KeyValue/KeyValues.dart';
 
 part 'Preferences.dart';
 
-T loadData<T>(Pref<T> pref)=> PrefManager.getVal(pref);
+T loadData<T>(Pref<T> pref) => PrefManager.getVal(pref);
 
-T? loadCustomData<T>(String key)=> PrefManager.getCustomVal(key);
+T loadCustomData<T>(String key) => PrefManager.getCustomVal(key);
 
-void saveData<T>(Pref<T> pref, T value)=> PrefManager.setVal(pref, value);
+Future<Rx<T>?> loadLiveCustomData<T>(String key) => PrefManager.getLiveCustomVal(key);
 
-void saveCustomData<T>(String key, T value)=> PrefManager.setCustomVal(key, value);
+void saveData<T>(Pref<T> pref, T value) => PrefManager.setVal(pref, value);
 
-void removeData(Pref<dynamic> pref)=> PrefManager.removeVal(pref);
+void saveCustomData<T>(String key, T value) => PrefManager.setCustomVal(key, value);
 
-void removeCustomData(String key)=> PrefManager.removeCustomVal(key);
-Rx<T?> loadLiveCustomData<T>(String key)=> PrefManager.getLiveCustomVal(key);
+void saveLiveCustomData<T>(String key, T value) => PrefManager.setLiveCustomVal(key, value);
+
+void removeData(Pref<dynamic> pref) => PrefManager.removeVal(pref);
+
+void removeCustomData(String key) => PrefManager.removeCustomVal(key);
 
 class Pref<T> {
   final Location location;
@@ -40,109 +45,181 @@ enum Location {
 }
 
 class PrefManager {
-  static Box? _generalPreferences;
-  static Box? _uiPreferences;
-  static Box? _playerPreferences;
-  static Box? _readerPreferences;
-  static Box? _irrelevantPreferences;
-  static Box? _protectedPreferences;
+  static Completer<Isar>? _generalPreferences;
+  static Completer<Isar>? _uiPreferences;
+  static Completer<Isar>? _playerPreferences;
+  static Completer<Isar>? _readerPreferences;
+  static Completer<Isar>? _irrelevantPreferences;
+  static Completer<Isar>? _protectedPreferences;
 
-  // Call this method at the start of the app
+  static final Map<Location, Map<String, dynamic>> _cache = {
+    Location.General: {},
+    Location.UI: {},
+    Location.Player: {},
+    Location.Reader: {},
+    Location.Irrelevant: {},
+    Location.Protected: {},
+  };
+
   static Future<void> init() async {
-    _hiveAdapters;
     if (_generalPreferences != null) return;
-    final path = await StorageProvider().getDirectory(subPath: 'preferences');
-    await Hive.initFlutter(path?.path);
-    _generalPreferences = await Hive.openBox('generalPreferences');
-    _uiPreferences = await Hive.openBox('uiPreferences');
-    _playerPreferences = await Hive.openBox('playerPreferences');
-    _readerPreferences = await Hive.openBox('readerPreferences');
-    _irrelevantPreferences = await Hive.openBox('irrelevantPreferences');
-    _protectedPreferences = await Hive.openBox('protectedPreferences');
+    final path = await StorageProvider().getDirectory(subPath: 'settings');
+
+    _generalPreferences = await _open('generalSettings', path!.path);
+    _uiPreferences = await _open('uiSettings', path.path);
+    _playerPreferences = await _open('playerSettings', path.path);
+    _readerPreferences = await _open('readerSettings', path.path);
+    _irrelevantPreferences = await _open('irrelevantSettings', path.path);
+    _protectedPreferences = await _open('protectedSettings', path.path);
+    removePref();
+    await _populateCache();
   }
 
-  static void _hiveAdapters() {
-    Hive.registerAdapter(ShowResponseAdapter());
-    Hive.registerAdapter(SelectedAdapter());
-    Hive.registerAdapter(ResponseTokenAdapter());
-    Hive.registerAdapter(PlayerSettingsAdapter());
+  static Future<Completer<Isar>> _open(String name, String directory) async {
+    final isar = await Isar.open(
+      [
+        KeyValueSchema,
+        PlayerSettingsSchema,
+        ResponseTokenSchema,
+        SelectedSchema,
+        ShowResponseSchema,
+      ],
+      directory: directory,
+      name: name,
+      inspector: false,
+    );
+    return Completer<Isar>()..complete(isar);
   }
 
-  static void setVal<T>(Pref<T> pref, T value) {
+  static Future<void> _populateCache() async {
+    for (var location in Location.values) {
+      final box = _getPrefBox(location);
+      final isar = await box?.future;
+      if (isar != null) {
+        final keyValues = await isar.keyValues.where().findAll();
+        for (var item in keyValues) {
+          _cache[location]?[item.key] = item.value;
+        }
+        final showResponse = await isar.showResponses.where().findAll();
+        for (var item in showResponse) {
+          _cache[location]?[item.key] = item;
+        }
+        final selected = await isar.selecteds.where().findAll();
+        for (var item in selected) {
+          _cache[location]?[item.key] = item;
+        }
+        final responseToken = await isar.responseTokens.where().findAll();
+        for (var item in responseToken) {
+          _cache[location]?[item.key] = item;
+        }
+        final playerSettings = await isar.playerSettings.where().findAll();
+        for (var item in playerSettings) {
+          _cache[location]?[item.key] = item;
+        }
+      }
+    }
+  }
+  static void setVal<T>(Pref<T> pref, T value) async {
     _checkInitialization();
+    _cache[pref.location]?[pref.key] = value;
+
     final box = _getPrefBox(pref.location);
-    box.put(pref.key, value);
+    final isar = await box?.future;
+
+    return _writeToIsar(isar, pref.key, value);
   }
 
   static T getVal<T>(Pref<T> pref) {
     _checkInitialization();
-    final box = _getPrefBox(pref.location);
-    final value = box.get(pref.key, defaultValue: pref.defaultValue);
-    if (value is T) {
-      return value;
-    } else if (value is Map) {
-      if (T == Map<String, bool>) {
-        return Map<String, bool>.from(value) as T;
-      }
+    if (_cache[pref.location]?.containsKey(pref.key) == true) {
+      return _cache[pref.location]![pref.key] as T;
     }
     return pref.defaultValue;
   }
 
-
-  static void setCustomVal<T>(String key, T value) {
+  static void setCustomVal<T>(String key, T value,
+      {Location location = Location.Irrelevant}) async {
     _checkInitialization();
-    final box = _getPrefBox(Location.Irrelevant);
-    box.put(key, value);
+    final box = _getPrefBox(location);
+    final isar = await box?.future;
+    _cache[location]?[key] = value;
+    return _writeToIsar(isar, key, value);
   }
 
-  static T? getCustomVal<T>(String key) {
+  static T? getCustomVal<T>(String key, {Location location = Location.Irrelevant}) {
     _checkInitialization();
-    final box = _getPrefBox(Location.Irrelevant);
-    final value = box.get(key);
-    if (value is T) {
-      return value;
-    } else if (value is Map) {
-      if (T == Map<String, bool>) {
-        return Map<String, bool>.from(value) as T;
-      }
-      if (T == Map<String, String>) {
-        return Map<String, String>.from(value) as T;
-      }
+    if (_cache[location]?.containsKey(key) == true) {
+      return _cache[location]![key] as T;
     }
     return null;
   }
 
-  static void removeVal(Pref<dynamic> pref) {
+  static void setLiveCustomVal<T>(String key, T value,{Location location = Location.Irrelevant}) async {
     _checkInitialization();
+    _cache[location]?[key] = value;
+    final box = _getPrefBox(location);
+    final isar = await box?.future;
+    final keyValue = KeyValue()
+      ..key = key
+      ..value = value;
+    isar?.keyValues.putSync(keyValue);
+  }
+
+  static Future<Rx<T>?> getLiveCustomVal<T>(String key, {Location location = Location.Irrelevant}) async {
+    _checkInitialization();
+    final box = _getPrefBox(location);
+    final isar = await box?.future;
+    final stream = Rx(isar?.keyValues.getByKeySync(key)?.value as T);
+    return stream;
+  }
+
+  static void removeVal(Pref<dynamic> pref) async {
+    _checkInitialization();
+    _cache[pref.location]?.remove(pref.key);
     final box = _getPrefBox(pref.location);
-    box.delete(pref.key);
+    final isar = await box?.future;
+    return isar?.writeTxn(() => isar.keyValues.deleteByKey(pref.key));
   }
-  static void removeCustomVal(String key) {
+
+  static void removeCustomVal(String key, {Location location = Location.Irrelevant}) async {
     _checkInitialization();
-    final box = _getPrefBox(Location.Irrelevant);
-    box.delete(key);
+    _cache[location]?.remove(key);
+    final box = _getPrefBox(location);
+    final isar = await box?.future;
+    return isar?.writeTxn(() => isar.keyValues.deleteByKey(key));
   }
-  static Rx<T?> getLiveCustomVal<T>(String key) {
-    _checkInitialization();
-    final box = _getPrefBox(Location.Irrelevant);
-    final value = getCustomVal<T>(key);
-    final observable = Rx<T?>(value);
+  
+  static Future<void> _writeToIsar<T>(Isar? isar, String key, T value) async {
+    if (isar == null) return;
 
-
-    box.listenable(keys: [key]).addListener(() {
-      final value = getCustomVal<T>(key);
-      observable.value = value;
+    await isar.writeTxn(() async {
+      if (value is ShowResponse) {
+        value.key = key;
+        await isar.showResponses.put(value);
+      } else if (value is Selected) {
+        value.key = key;
+        await isar.selecteds.put(value);
+      } else if (value is ResponseToken) {
+        value.key = key;
+        await isar.responseTokens.put(value);
+      } else if (value is PlayerSettings) {
+        value.key = key;
+        await isar.playerSettings.put(value);
+      } else {
+        final keyValue = KeyValue()..key = key..value = value;
+        await isar.keyValues.put(keyValue);
+      }
     });
-    return observable;
   }
-
   static void _checkInitialization() {
     if (_generalPreferences == null) {
-      throw Exception('Hive not initialized. Call PrefManager.init() first.');
+      throw Exception(
+          'Preferences not initialized. Call PrefManager.init() first.',
+      );
     }
   }
 
-  static Box _getPrefBox(Location location) {
+  static Completer<Isar>? _getPrefBox(Location location) {
     switch (location.name) {
       case 'General':
         return _generalPreferences!;
@@ -159,5 +236,12 @@ class PrefManager {
       default:
         throw Exception("Invalid box name");
     }
+  }
+}
+
+void removePref(){
+  if (loadCustomData('once1') ?? true){
+    removeData(PrefName.simklHomeLayout);
+    saveCustomData('once1', false);
   }
 }
